0c45a2bc43eb9499aab4b378b6af5f5ac0f24624
Jane <jenya.kissel@gmail.com>
2025-11-25 12:14:10 +0100

    More data retrieval operations + plotting speed vs distance for lap

--- python/get_data.py
+++ python/get_data.py
@@ -19,8 +19,6 @@ ver_laps = laps.pick_drivers(['1']) # Max Verstappen
 ant_laps = ant_laps.reset_index(drop=True)
 ver_laps = ver_laps.reset_index(drop=True)
 
-fastest_ant = ant_laps.sort_values(by='LapTime').head(1)
-
 lec_lap5 = lec_laps[lec_laps['LapNumber'] == 5]
 lec_lap5_time = lec_lap5['LapTime'].iloc[0]
 
@@ -37,18 +35,58 @@ laps = session.laps
 ant_laps = laps.pick_drivers(['12']) # Kimi Antonelli
 lec_laps = laps.pick_drivers(['16']) # Charles Leclerc
 ver_laps = laps.pick_drivers(['1']) # Max Verstappen
+rus_laps = laps.pick_drivers(['63']) # George Russel
 
 ant_laps = ant_laps.reset_index(drop=True)
+ver_laps = ver_laps.reset_index(drop=True)
 
-# retrieving the fastest lap
+# retrieving the fastest lap for each driver
 fastest_ant = ant_laps.sort_values(by='LapTime').head(1)
+fastest_lec = lec_laps.sort_values(by='LapTime').head(1)
+fastest_ver = ver_laps.sort_values(by='LapTime').head(1)
+fastest_rus = rus_laps.sort_values(by='LapTime').head(1)
+
+# retrieve the fastest lap per driver
+fastest_per_driver = laps.groupby('Driver')['LapTime'].min()
+
+# the fastest general lap in the session
+session_best = laps.pick_fastest()
 
 ant_lap48 = ant_laps[ant_laps['LapNumber'] == 48].iloc[0]
+lec_lap43 = lec_laps[lec_laps['LapNumber'] == 43].iloc[0]
+ver_lap50 = ver_laps[ver_laps['LapNumber'] == 50].iloc[0]
+rus_lap50 = rus_laps[rus_laps['LapNumber'] == 50].iloc[0]
 
 # getting telemetry for that lap
 ant_lap48_tel = ant_lap48.get_telemetry()
 
 ant_speed = ant_lap48_tel['Speed']
 
+# retrieve all laps on soft tyres
+soft_laps = laps[laps['Compound'] == 'SOFT']
+
+# retrieve average lap time per stint
+laps_avg = laps.groupby(['Driver', 'Stint'])['LapTime'].mean()
+
 # print(ant_laps[['LapNumber', 'LapTime', 'Position']].tail(35))
-print(ant_speed.head())
+# print(ant_speed.head())
+
+# checking out plotting
+
+# getting telemetry for 50th lap
+rus_lap50_tel = rus_lap50.get_car_data().add_distance()
+
+import matplotlib.pyplot as plt
+import fastf1.plotting
+
+fastf1.plotting.setup_mpl(color_scheme='fastf1')
+
+plt.figure(figsize=(12, 6))
+plt.plot(rus_lap50_tel['Distance'], rus_lap50_tel['Speed'])
+
+plt.title("Russel - Speed vs Distance on Lap 50")
+plt.xlabel("Distance (m)")
+plt.ylabel("Speed (km/h)")
+plt.grid = True
+
+plt.show()
